1f7ddbef4b356158b7b184178a82f1e4547d3267
Nataliyayaya <natalimatash@gmail.com>
2022-11-05 21:43:49 +0200

    Added style

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,18 +1,19 @@
+import './WeatherInfo.css'
 
 export default function WeatherInfo(){
    return(
       <div className="WeatherInfo">
          <div>
-         <h1 className="city">Kyiv</h1>
-         <ul>
-            <li>Sunday 10:25</li>
-            <li>Fog</li>
-            <li>Humidity: 80%</li>
-            <li>Wind: 2km/h</li>
-         </ul>
-         <div>
-            <span>9&deg;C</span>
-         </div>
+           <h1 className="city">Kyiv</h1>
+           <div className='city__info'>
+            <ul>
+               <li>Sunday 10:25</li>
+               <li>Fog</li>
+               <li>Humidity: 80%</li>
+               <li>Wind: 2km/h</li>
+            </ul>
+            <span>9<sup>&deg;C</sup></span>
+           </div>
          </div>
       </div>
    )

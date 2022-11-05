26ac61b2c22a78abe42fbc777741fbac84419e9b
Nataliyayaya <natalimatash@gmail.com>
2022-11-05 21:29:58 +0200

    Create main structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
+import WeatherInfo from './WeatherInfo';
 
 function App() {
   return (
     <div className="App">
+      <div className='container'>
+      <div className='app__content'>
+        <form>
+          < div className='row'>
+            <div className='col-9'>
+            <input type="search" class="form-control" placeholder='Type your city...'/>
+            </div>
+            <div className='col-3'>
+            <input type="submit" class="btn btn-primary" value='Search'/>
+            </div>
+          </div>
+        </form>
+        <WeatherInfo />
+      </div>
+      </div>
     </div>
   );
 }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,19 @@
+
+export default function WeatherInfo(){
+   return(
+      <div className="WeatherInfo">
+         <div>
+         <h1 className="city">Kyiv</h1>
+         <ul>
+            <li>Sunday 10:25</li>
+            <li>Fog</li>
+            <li>Humidity: 80%</li>
+            <li>Wind: 2km/h</li>
+         </ul>
+         <div>
+            <span>9&deg;C</span>
+         </div>
+         </div>
+      </div>
+   )
+}

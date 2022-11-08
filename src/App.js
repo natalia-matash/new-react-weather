902405bbdca2590a3fd32d76c5454688b3b0f1b6
Nataliyayaya <natalimatash@gmail.com>
2022-11-08 10:57:36 +0200

    Create footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Weather from './Weather'
+import './App.css';
 
 
  export default function App() {
@@ -7,6 +8,9 @@ import Weather from './Weather'
     <div className='App'>
       <div className='container'>
         <Weather city='Kyiv' />
+        <footer>
+          <p>This project was coded <strong>by Nataliyayaya</strong> and is <a href='https://github.com/Nataliyayaya/new-react-weather' target="_blank" without rel="noreferrer">open-sourced by GitHub</a></p>
+        </footer>
       </div>
     </div>
   );
